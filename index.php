89b54d2475e14a07ea98be019b5dbc5f35581d89
Luuducq <luuquangduc04012001@gmail.com>
2024-07-23 19:38:38 +0700

    xinchao

--- index.php
+++ index.php
@@ -11,8 +11,6 @@
  $spnew=loadall_sanpham_home();
  $dsdm=loadall_danhmuc();
  $dstop10=loadall_sanpham_top10();
-
-
  if((isset($_GET['act']))&&($_GET['act']!="")){
    $act=$_GET['act'];
    switch ($act) {
