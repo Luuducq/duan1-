a1ea231ff7b0dfeff25211208d505be2b1b6f380
Luuducq <luuquangduc04012001@gmail.com>
2024-07-27 12:30:05 +0700

    kk

--- admin/donhang/update.php
+++ admin/donhang/update.php
@@ -6,9 +6,16 @@
 ?>
     <div class="row">
             <div class="row fromtitle"><h1>Cập nhật tình trạng đơn hàng</h1></div>
+            <div class="row ">
+                                       <h1>Ghi chú:</h1>
+                                       <h1>0: Đơn hàng mới</h1>
+                                       <h1>1: Đang xử lý</h1>
+                                       <h1>2: Đang giao hàng</h1>
+                                       <h1>3: Hoàn tất</h1>
+            </div>
             <div class="row fromcontent">
             <form action="index.php?act=updatedh&id=<?=$id?>" method="post" >
-                
+                                
                  <div class="row mb10">
                     Tình trạng đơn hàng mới :
                     <input type="text" name="billstatus" value="<?=$billstatus?>" >
